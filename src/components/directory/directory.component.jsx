94fd969b874e0bf2dbfeb8f67193ad1fe54f6e18
Kennith Nichol <ken.nichol@live.ca>
2020-01-22 10:00:09 -0800

    update our menu-items to navigation withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,54 +1,59 @@
-import React from 'react';
-import MenuItem from '../menu-item/menu-item.component';
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component";
 
-import './directory.styles.scss';
+import "./directory.styles.scss";
 
 class Directory extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                    id: 4,
-                    size: 'large'
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
-                    id: 5,
-                    size: 'large'
-                },
-            ]
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1,
+          linkUrl: "hats"
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+          linkUrl: ""
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3,
+          linkUrl: ""
+        },
+        {
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          id: 4,
+          linkUrl: "",
+          size: "large"
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/mens.png",
+          id: 5,
+          linkUrl: "",
+          size: "large"
         }
-    }
+      ]
+    };
+  }
 
-    render() {
-        return (
-            <div className='directory-menu'>
-                {this.state.sections.map( ({ title, imageUrl, id, size }) => (
-                        <MenuItem title={title} key={id} imageUrl={imageUrl} size={size} />
-                ))}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Directory;
